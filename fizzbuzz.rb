bd168d0c64dbcfff0b5a817eae62bd8f19b539d1
Jathmel Gordon <jathmel@gmail.com>
2018-04-14 22:57:11 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,11 @@
 def fizzbuzz(num)
-  if num % 15
+  if num % 15 == 0
     "FizzBuzz"
-  elsif num % 5 == 0 
-    
+  elsif num % 5 == 0
+    "Buzz"
+  elsif num % 3 == 0
+    "Fizz"
+  else 
+    nil 
+  end
 end
